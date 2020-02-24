132d76674b05fcd22ca39700135177bb32c4687b
aahad91 <abdulahadayaz@hotmail.com>
2020-02-24 17:36:37 +0100

    modified start script

--- vm-images/mitmproxy/start.sh
+++ vm-images/mitmproxy/start.sh
@@ -2,6 +2,20 @@
 
 date > /tngbench_share/start.txt
 
-mitmproxy --allow_hosts --anticache
+while true;
+do
+ case "$1"
+ in
+ mp.input) shift;INPUT=$1;;
+ mp.output) shift;OUTPUT=$1;;
+ vnf) shift;VNF=$1;;
+ esac
+ shift;
+ if [ "$1" = "" ]; then
+  break
+ fi
+done
+
+mitmproxy --set allow_hosts:[$INPUT] anticache:[True]
 
 echo "mitmproxy VNF started"
